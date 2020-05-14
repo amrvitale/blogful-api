3f108d9b740917989da93dfe2e7793ab958dc828
Angela Bumgarner <amrvitale@gmail.com>
2020-05-13 19:00:59 -0700

    blogful until migratetest issue

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
 
 const app = express()
 
@@ -15,9 +16,16 @@ app.use(morgan(morganOption))
 app.use(cors())
 app.use(helmet())
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
+
+app.get('/articles', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getAllArticles(knexInstance)
+    .then(articles => {
+      res.json(articles)
+    })
+      .catch(next)
+    })
+
 
   app.use(function errorHandler(error, req, res, next) {
       let response
